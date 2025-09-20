556143d7b93030a4c1ad09ac6f172c407094a35a
BAAKJIMIN <baakjimin@gmail.com>
2025-09-20 10:44:29 +0900

    배포용 테스트

--- src/scripts/db.js
+++ src/scripts/db.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-  host: 'localhost',
+  host: 'hirachan',
   user: 'root',
   password: 'Qwe067901!',
   database: 'hirachan',
