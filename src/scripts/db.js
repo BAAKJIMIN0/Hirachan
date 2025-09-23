da3b367dcb4feb919921b58fbe69791fa78ebf4d
BAAKJIMIN <baakjimin@gmail.com>
2025-09-23 17:14:26 +0900

    개발/배포 DB 분리

--- src/scripts/db.js
+++ src/scripts/db.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'hirachan',
-  password: 'Qwe067901!',
-  database: 'hirachan',
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 100,
   queueLimit: 0,
